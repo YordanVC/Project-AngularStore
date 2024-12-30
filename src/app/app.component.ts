28a8391d374667697086ae5ef6baacaa54da6297
Yordan Valdiviezo <yordan.2511@gmail.com>
2024-12-30 12:36:42 -0500

    cambios de products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{ Product }from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +9,23 @@ export class AppComponent {
   title = 'platzi-store';
 
   items=['yordan','maria','andy']
+
+  products: Product[]=[
+    {
+      id: '1',
+      image: 'assets/images/camisaAzul.png',
+      title: 'Camiseta',
+      price: 20,
+      description: 'una camisa azul'
+    },
+    {
+      id: '2',
+      image: 'assets/images/camisaCivil.png',
+      title: 'Camiseta Guarida Civil',
+      price: 20,
+      description: 'una camisa de guardia civil'
+    }
+  ]
 
   addItem(){
     this.items.push('nuevo Item');
